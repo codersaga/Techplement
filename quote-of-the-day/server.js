87dabced2e03752c3832783b79d365922659040c
codersaga <codersaga@gmail.com>
2024-10-08 22:34:01 +0530

    Update server.js

--- quote-of-the-day/server.js
+++ quote-of-the-day/server.js
@@ -23,7 +23,7 @@ app.get('/quote', async (req, res) => {
         : 'https://api.api-ninjas.com/v1/quotes';
 
         const response = await axios.get(apiUrl,{
-            headers: { 'X-Api-Key': API_KEY }
+            headers: { 'X-Api-Key': process.env.API_KEY }
         });
 
         if (response.data && response.data.length > 0) {
